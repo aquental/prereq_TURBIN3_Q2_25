08af4d2b6f7e2acb82cf9daa3f2b68fddd3c2bd8
Antonio Quental <antonio.quental@gmail.com>
2025-03-26 14:55:24 -0300

    final solution

--- README.md
+++ README.md
@@ -5,13 +5,13 @@
 **Cohort Q2** / 2025
 
 ### _Typescript_ - Prerequisites: Enrollment dApp
-> 
-> [program idl](https://explorer.solana.com/address/WBAQSygkwMox2VuWKU133NxFrpDZUBdvSBeaBEue2Jq?cluster=devnet)
+
+> [program idl](https://explorer.solana.com/address/Trb3aEx85DW1cEEvoqEaBkMn1tfmNEEEPaKzLSu4YAv?cluster=devnet)
 >
-> [devnet-transaction](https://explorer.solana.com/tx/3Ji3KRS8yiWnWY46if7mwTB27hfudk6mnBE3kRFNuM3U7qnAyouyDY7H6zWLsd1ZufDt7UUVeRc1J8a1yNCeMU8H?cluster=devnet)
+> [devnet-transaction](https://explorer.solana.com/tx/4VBMJLG3QWExxuyE91DhjxpahnExxgpFLetPoGg64XXUwuytZxSTuheT1s7KXxbFM4ukrWGFgFavgqkZQTSug2gG?cluster=devnet)
 
 ### _Rust_ - Prerequisites: Enrollment dApp
+
+> link ?
 >
-> link
->
-> devnet trx
+> devnet trx ?
--- typescript/enroll.ts
+++ typescript/enroll.ts
@@ -3,10 +3,14 @@ import { Program, Wallet, AnchorProvider } from "@coral-xyz/anchor";
 import { IDL, Turbin3Prereq } from "./programs/Turbin3_prereq";
 import wallet from "./turbin3-wallet.json";
 
+const ENDPOINT =
+  "https://polished-warmhearted-frog.solana-devnet.quiknode.pro/9bc0c3437243817577c59c3690d3bcde03fe8b6f/";
+//const ENDPOINT = "https://api.devnet.solana.com";
+
 // We're going to import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 // Create a devnet connection
-const connection = new Connection("https://api.devnet.solana.com");
+const connection = new Connection(ENDPOINT);
 // Github account
 const github = Buffer.from("aquental", "utf8");
 // Create our anchor provider
@@ -17,7 +21,7 @@ const provider = new AnchorProvider(connection, new Wallet(keypair), {
 const program: Program<Turbin3Prereq> = new Program(IDL, provider);
 
 // Create the PDA for our enrollment account
-const enrollment_seeds = [Buffer.from("pre"), keypair.publicKey.toBuffer()];
+const enrollment_seeds = [Buffer.from("prereq"), keypair.publicKey.toBuffer()];
 const [enrollment_key, _bump] = PublicKey.findProgramAddressSync(
   enrollment_seeds,
   program.programId
