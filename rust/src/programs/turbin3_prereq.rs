bf93df0dbd0ad618fa7bd5ee72b4904c233d261c
Antonio Quental <antonio.quental@gmail.com>
2025-03-27 17:51:02 -0300

    IDL from ADcaide4vBtKuyZQqdU689YqEGZMCmS4tL35bdTv9wJa

--- rust/src/programs/turbin3_prereq.rs
+++ rust/src/programs/turbin3_prereq.rs
@@ -1,60 +1,28 @@
 use solana_idlgen::idlgen;
 
-// Anchor IDL version 0.30.1
-idlgen!(
-{
-  "address": "ADcaide4vBtKuyZQqdU689YqEGZMCmS4tL35bdTv9wJa",
+idlgen!({
+  "version": "0.1.0",
+  "name": "Turbin3_prereq",
   "metadata": {
-    "name": "turbine_prereq",
-    "version": "0.1.0",
-    "spec": "0.1.0",
-    "description": "Created with Anchor"
-  },
+"address": "ADcaide4vBtKuyZQqdU689YqEGZMCmS4tL35bdTv9wJa"},
   "instructions": [
     {
       "name": "complete",
-      "discriminator": [
-        0,
-        77,
-        224,
-        147,
-        136,
-        25,
-        88,
-        76
-      ],
       "accounts": [
         {
           "name": "signer",
-          "writable": true,
-          "signer": true
+          "isMut": true,
+          "isSigner": true
         },
         {
           "name": "prereq",
-          "writable": true,
-          "pda": {
-            "seeds": [
-              {
-                "kind": "const",
-                "value": [
-                  112,
-                  114,
-                  101,
-                  114,
-                  101,
-                  113
-                ]
-              },
-              {
-                "kind": "account",
-                "path": "signer"
-              }
-            ]
-          }
+          "isMut": true,
+          "isSigner": false
         },
         {
-          "name": "system_program",
-          "address": "11111111111111111111111111111111"
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
         }
       ],
       "args": [
@@ -66,29 +34,21 @@ idlgen!(
     },
     {
       "name": "update",
-      "discriminator": [
-        219,
-        200,
-        88,
-        176,
-        158,
-        63,
-        253,
-        127
-      ],
       "accounts": [
         {
           "name": "signer",
-          "writable": true,
-          "signer": true
+          "isMut": true,
+          "isSigner": true
         },
         {
           "name": "prereq",
-          "writable": true
+          "isMut": true,
+          "isSigner": false
         },
         {
-          "name": "system_program",
-          "address": "11111111111111111111111111111111"
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
         }
       ],
       "args": [
@@ -101,29 +61,7 @@ idlgen!(
   ],
   "accounts": [
     {
-      "name": "SolanaCohort5Account",
-      "discriminator": [
-        167,
-        81,
-        85,
-        136,
-        32,
-        169,
-        137,
-        77
-      ]
-    }
-  ],
-  "errors": [
-    {
-      "code": 6000,
-      "name": "InvalidGithubAccount",
-      "msg": "Invalid Github account"
-    }
-  ],
-  "types": [
-    {
-      "name": "SolanaCohort5Account",
+      "name": "PrereqAccount",
       "type": {
         "kind": "struct",
         "fields": [
@@ -133,11 +71,17 @@ idlgen!(
           },
           {
             "name": "key",
-            "type": "pubkey"
+            "type": "publicKey"
           }
         ]
       }
     }
+  ],
+  "errors": [
+    {
+      "code": 6000,
+      "name": "InvalidGithubAccount",
+      "msg": "Invalid Github account"
+    }
   ]
-}
-);
+});
